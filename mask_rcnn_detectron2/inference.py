c5779c2f63552a9b57874556aae6c26fa81c058c
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-04-21 18:12:57 -0400

    added device arg

--- mask_rcnn_detectron2/inference.py
+++ mask_rcnn_detectron2/inference.py
@@ -1,53 +1,61 @@
 import argparse
-import cv2
 import glob
 import os
 import os.path as osp
 import time
-import tqdm
 
+import cv2
+import detectron2.data.transforms as T
+import tqdm
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
-from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.catalog import Metadata
 from detectron2.data.datasets import register_coco_instances
-import detectron2.data.transforms as T
 from detectron2.engine import DefaultPredictor
-from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.utils.visualizer import ColorMode, Visualizer
 
-
-my_metadata = Metadata()
-my_metadata.set(thing_classes=["dog", "cat"])
-
-CONFIG = "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
+R_101_CONFIG = "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"
+R_50_CONFIG = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
 THIS_DIR = osp.dirname(osp.abspath(__file__))
-CLASSES_TXT = osp.join(THIS_DIR, "../classes.txt")
+CLASSES_TXT = osp.join(THIS_DIR, "classes.txt")
 
 IMAGE_EXTENSIONS = ["jpg", "jpeg", "png"]
 VIDEO_EXTENSIONS = ["mp4", "mov"]
 
 
 class MaskRcnnInference:
-    def __init__(self, weights_file, coco_json=None, score_thresh=0.7):
+    def __init__(
+        self,
+        weights_file,
+        coco_json=None,
+        min_size_test=None,
+        score_thresh=0.7,
+        config_path=R_101_CONFIG,
+        device=None,
+    ):
         self.weights_file = weights_file
         self.coco_json = coco_json
         self.score_thresh = score_thresh
 
         self.metadata = self.generate_metadata()
-        self.cfg = self.generate_cfg()
+        self.cfg = self.generate_cfg(config_path)
+        if device is not None:
+            self.cfg.MODEL.DEVICE = device
         print("Loading predictor...")
         self.predictor = DefaultPredictor(self.cfg)
         print("Predictor loaded.")
 
         # Get resize transform (stolen from detectron2 COCOEvaluator)
-        self.resize_edge = T.ResizeShortestEdge(short_edge_length=800, max_size=1333)
+        self.resize_edge = T.ResizeShortestEdge(
+            short_edge_length=min_size_test, max_size=1333
+        )
 
     def generate_metadata(self):
         if not osp.isfile(CLASSES_TXT):
             assert self.coco_json is not None, (
                 f"{CLASSES_TXT} does not exist; locate COCO format train dataset json "
                 "and re-run with -j command line arg to generate it automatically."
-                "Or, manually create the file by listing class names on each line."
             )
             print("Parsing JSON file to determine classes list...")
             dataset_name = "test"
@@ -61,15 +69,15 @@ class MaskRcnnInference:
 
         metadata = Metadata()
         with open(CLASSES_TXT) as f:
-            thing_classes = [i for i in f.read().splitlines() if i != ""]
+            thing_classes = f.read().splitlines()
         print(f"Classes have been read from {CLASSES_TXT}.")
         metadata.set(thing_classes=thing_classes)
 
         return metadata
 
-    def generate_cfg(self):
+    def generate_cfg(self, config_path):
         cfg = get_cfg()
-        cfg.merge_from_file(model_zoo.get_config_file(CONFIG))
+        cfg.merge_from_file(model_zoo.get_config_file(config_path))
         cfg.MODEL.WEIGHTS = self.weights_file
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.score_thresh
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(self.metadata.thing_classes)
@@ -114,6 +122,9 @@ def main():
         help="where visualized inferences are saved (default: test_output)",
         default="test_output",
     )
+    parser.add_argument("--shrink", action="store_true")
+    parser.add_argument("--normal", action="store_true")
+    parser.add_argument("--fast", action="store_true")
     args = parser.parse_args()
 
     weights_path = args.weights_path
@@ -124,7 +135,19 @@ def main():
     assert osp.isdir(test_data) or osp.isfile(test_data), f"{test_data} does not exist!"
     os.makedirs(output_dir, exist_ok=True)
 
-    mask_rcnn = MaskRcnnInference(weights_path, train_coco_json)
+    config_path = R_50_CONFIG if args.fast else R_101_CONFIG
+    if args.normal:
+        min_size_test = 480
+    elif args.shrink:
+        min_size_test = 240
+    else:
+        min_size_test = None
+    mask_rcnn = MaskRcnnInference(
+        weights_path,
+        train_coco_json,
+        min_size_test=min_size_test,
+        config_path=config_path,
+    )
 
     # Determine if input path is an image, directory, or video
     if osp.isdir(test_data):
@@ -145,7 +168,7 @@ def main():
             img = cv2.imread(img_path)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             vis_img = mask_rcnn.visualize_inference(img)
-            output_path = osp.join(output_dir, f'{idx:04}.png')
+            output_path = osp.join(output_dir, f"{idx:04}.png")
             cv2.imwrite(output_path, vis_img)
     else:
         vid = cv2.VideoCapture(test_data)
@@ -156,6 +179,10 @@ def main():
         for idx in pbar:
             _, frame = vid.read()
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # img = frame
             vis_img, pred_time = mask_rcnn.visualize_inference(img, get_pred_time=True)
 
             # Add pred_time to progress bar
@@ -163,12 +190,14 @@ def main():
 
             # Use first frame to set up output video
             if idx == 0:
-                out_vid_path = osp.join(output_dir, osp.basename(test_data))
-                four_cc = cv2.VideoWriter_fourcc(*'MP4V')
+                weights_dir = osp.dirname(osp.abspath(weights_path))
+                out_vid_path = osp.join(weights_dir, osp.basename(test_data))
+                four_cc = cv2.VideoWriter_fourcc(*"MP4V")
                 fps = vid.get(cv2.CAP_PROP_FPS)
                 height, width = vis_img.shape[:2]
                 out_vid = cv2.VideoWriter(out_vid_path, four_cc, fps, (width, height))
             out_vid.write(vis_img)
+        print(f"Output video was saved to {out_vid_path}")
 
 
 if __name__ == "__main__":
